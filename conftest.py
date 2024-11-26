317ef31d8022e14dccb1e87e8615816771b6cc3d
manikosto-qa <manikosto@mail.ru>
2024-11-26 18:21:15 +0100

    First commit

---
+++ conftest.py
@@ -0,0 +1,34 @@
+import os
+
+import pytest
+from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+
+
+def set_options():
+    if os.environ["BROWSER"] == "chrome":
+        options = webdriver.ChromeOptions()
+    elif os.environ["BROWSER"] == "firefox":
+        options = webdriver.FirefoxOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    return options
+
+@pytest.fixture(scope="function", autouse=True)
+def driver(request):
+    if os.environ["BROWSER"] == "chrome":
+        driver = webdriver.Chrome(options=set_options())
+    elif os.environ["BROWSER"] == "firefox":
+        driver = webdriver.Firefox(options=set_options())
+    elif os.environ["BROWSER"] == "remote":
+        driver = webdriver.Remote(
+            command_executor='http://selenium-hub:4444/wd/hub',
+            options=webdriver.ChromeOptions()
+        )
+    request.cls.driver = driver
+    yield driver
+    driver.delete_all_cookies()
+    driver.quit()
